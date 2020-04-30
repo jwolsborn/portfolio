fddd74e01a68552efa17a920dcae0b027bd533fa
Josh Wolsborn <wolsborn@pdx.edu>
2020-04-30 12:49:56 -0700

    Add home page styles

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import picture from './josh.jpeg';
+import './Home.css';
 
 class Home extends Component {
     render() {
         return (
             <div className="Home">
                 <h1>Welcome to the Josh Wolsborns Portfolio Home Page!</h1>
-                <img src='josh.jpeg' alt="Josh's picture"/>
+                <img src={picture} alt="me"/>
             </div>
         );
     }

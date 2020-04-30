07c047c7e395f71b70c7f89fe4fe7a2c229fc60e
Josh Wolsborn <wolsborn@pdx.edu>
2020-04-30 10:19:49 -0700

    Add menu bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Layout } from 'antd';
+import LinkMenu from './Menu.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer, Sider } = Layout;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  handleCollapse = collapsed => {
+    console.log('Action', { collapsed });
+    this.setState({ collapsed });
+  };
+
+  render () {
+    return (
+        <HashRouter>
+          <Layout className="layout">
+            <Sider
+                collapsible
+                collapsed={this.state.collapsed}
+                onCollapse={this.handleCollapse}
+                >
+              <div className="logo" />
+              <LinkMenu location={this.location}/>
+            </Sider>
+            <Layout>
+            <Header className="header" />
+            <Content className="aligner content">
+                <Switch>
+                  <Route exact path="/" render={() => <Redirect to="/home" />} />
+                </Switch>
+            </Content>
+            <Footer className="footer">Ant Design ©2016 Created by Ant UED</Footer>
+            </Layout>
+          </Layout>
+        </HashRouter>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Menu.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Menu, Icon } from 'antd';
+
+const Linkmenu = withRouter(props => {
+    const { location } = props;
+    return (
+        <Menu
+            theme="dark"
+            mode="inline"
+            selectedKeys={[location.pathname]}
+            defaultSelectedKeys={['/home']}
+        >
+            <Menu.Item key="/home">
+                <Icon type="home" />
+                <span>Home</span>
+                <Link to="/home" />
+            </Menu.Item>
+            <Menu.Item key="/about-me">
+                <Icon type="user-add" />
+                <span>Add Faculty</span>
+                <Link to="/about-me" />
+            </Menu.Item>
+            <Menu.Item key="/projects">
+                <Icon type="usergroup-add" />
+                <span>Add Committee</span>
+                <Link to="/projects" />
+            </Menu.Item>
+            <Menu.Item key="/contact">
+                <Icon type="user" />
+                <span>Faculty Info</span>
+                <Link to="/contact" />
+            </Menu.Item>
+        </Menu>
+    );
+});
+
+export default Linkmenu;

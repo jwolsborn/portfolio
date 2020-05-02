f692ac62a26abeefb53a010c3fa55d1441bf9bca
Josh Wolsborn <wolsborn@pdx.edu>
2020-05-01 20:03:35 -0700

    Add contact component

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
-
+import { Form, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
+import './Contact.css';
 class Contact extends Component {
+
+
     render(){
+        const layout = {
+            labelCol: {
+                span: 8,
+            },
+            wrapperCol: {
+                span: 16,
+            },
+        };
+        const validateMessages = {
+            required: '${label} is required!',
+            types: {
+                email: '${label} is not validate email!',
+            }
+        };
+
+        const onFinish = values => {
+            console.log(values);
+        };
+
         return(
-            <div></div>
+            <div className="contact">
+                <h1>Contact Me</h1>
+                <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+                    <Form.Item
+                        name={['user', 'name']}
+                        label="Name"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['user', 'email']}
+                        label="Email"
+                        rules={[
+                            {
+                                type: 'email',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name={['user', 'Message']} label="Message">
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         );
     }
 }
